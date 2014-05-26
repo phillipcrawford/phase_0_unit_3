5c20dcb1e2b97f7a535d9b2b42c14ff04fd9934c
Phillip P. Crawford <phillip.crawford3@gmail.com>
2014-05-26 00:17:04 -0700

    ruby2

--- week_8_and_9/3_Ruby/reverse_words/my_solution.rb
+++ week_8_and_9/3_Ruby/reverse_words/my_solution.rb
@@ -1,27 +1,50 @@
 # U3.W8-9: Reverse Words
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
+# we want to take a sentence
+# split it into words
+# reverse each word
+# make sentence of reversed words
+# return the sentence
 
 
 # 3. Initial Solution
-
-
+# friend = Array.new
+# words = "THIS IS MY TEST STRING".split(" ")
+# words.each {|x| friend.push(x)}
+# er = friend.length
+# for i in 0...er
+#   puts friend[i].reverse
+# end
 
 
 # 4. Refactored Solution
 
-
+def reverse_words(input)
+  if (input == "")
+  ""
+  end	
+  friend = Array.new	
+  words = input.split(" ")
+  words.each {|x| friend.push(x)}
+  s = ""
+  for i in 0...((friend.length) -1)
+    s = s + friend[i].reverse + " "
+  end
+  s = s + friend[friend.length-1].reverse
+  s	
+end
 
 
 
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
-
+puts reverse_words("THIS IS MY TEST STRING") == "SIHT SI YM TSET GNIRTS"
 
 
 
